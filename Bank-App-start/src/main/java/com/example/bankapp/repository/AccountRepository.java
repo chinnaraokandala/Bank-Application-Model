26f3ee4a3223f19193f40b435183cad7e287301d
chinnaraokandala <chinnastylish34@gmail.com>
2025-05-16 11:22:23 +0530

    First Commit

---
+++ Bank-App-start/src/main/java/com/example/bankapp/repository/AccountRepository.java
@@ -0,0 +1,13 @@
+package com.example.bankapp.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.bankapp.model.Account;
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long>{
+	Optional<Account> findByUsername(String username);
+
+}
